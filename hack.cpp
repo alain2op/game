0d9c485f9c90e7d1615bf47222515308fd10731e
alain2op <cbsujanreddy@gmail.com>
2024-07-27 22:47:05 +0530

    added updated files

--- hack.cpp
+++ hack.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// bool a=false;
 void World::Tick(float f){
     ClientWorld* world = *((ClientWorld**)(dlsym(RTLD_NEXT, "GameWorld")));
     GameAPI* game=*((GameAPI**)(dlsym(RTLD_NEXT,"Game")));
@@ -19,7 +20,15 @@ void World::Tick(float f){
     player->m_walkingSpeed = 99999;
     player->m_jumpSpeed = 999;
     player->m_jumpHoldTime = 99999;
+    // player->SetPosition(Vector3(10000,10000,10000));
+    // if(a){
+    //     float x=-7894;
+    //     float y=64482;
+    //     float z=2463;
+    //     player->SetPosition(Vector3(x,y,z));
+    // }
 }
+
 void Player::Chat(const char* message){
     char buffer[256];
     strncpy(buffer, message, sizeof(buffer));
@@ -32,7 +41,6 @@ void Player::Chat(const char* message){
         token = strtok(NULL, " ");
     }
      if (word_count == 4 && strcmp(words[0], "tpr") == 0) {
-        cout << "tpr" << endl;
         Vector3 pos = this->GetPosition();
         float x = strtof(words[1], NULL);
         float y = strtof(words[2], NULL);
@@ -44,13 +52,32 @@ void Player::Chat(const char* message){
         float y = strtof(words[2], NULL);
         float z = strtof(words[3], NULL);
         this->SetPosition(Vector3(x, y, z));
-        cout << "tp" << endl;
+    }
+    else if(word_count==1 && strcmp(words[0],"a")==0){
+        ClientWorld* world = *((ClientWorld**)(dlsym(RTLD_NEXT, "GameWorld")));
+        for(ActorRef<IActor> actor_s:world->m_actors){
+            Vector3 position=actor_s.m_object->GetLookPosition();
+            printf("%s\n",actor_s.m_object->GetDisplayName());
+            std::cout<<position.x<<'\t'<<position.y<<'\t'<<position.z<<endl;
+        }
+        std::cout<<endl<<endl;
     }
     else if(word_count==1 && strcmp(words[0],"q")==0){
-        cout<<"done";
-        IQuest* iquest=this->m_currentQuest;
-        this->CompleteQuest(iquest);
-    }
-    
+        // if(a==false){a=true;}
+        // else{a=false;}
+        // BearChest* bearchest;
+        // cout<<"in";
+        // for(ActorRef<IActor> actor_s:world->m_actors){
+        //     Vector3 position=actor_s.m_object->GetLookPosition();
+        //     if(abs(position.z-2663)<=5){
+        //         bearchest=(BearChest*)actor_s.m_object;
+        //         cout<<position.z;
+        //         std::map<ActorRef<IPlayer>, float> m_playerTimeLeft=bearchest->m_playerTimeLeft;
+        //         for (auto& pair : m_playerTimeLeft) {
+        //             pair.second = 1;
+        //         }
+        //     }
+        // }
+        
+    }   
 }
-
