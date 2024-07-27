627c6e1fdfc2cf425b0286b415533200e4d66414
alain2op <cbsujanreddy@gmail.com>
2024-07-27 22:51:13 +0530

    removed a small error

--- newlibGameLogic.h
+++ newlibGameLogic.h
@@ -1177,7 +1177,7 @@ class BearChest : public Actor {
     class std::vector<ActorRef<Bear>, std::allocator<ActorRef<Bear> > > m_bears;
     class std::map<ActorRef<IPlayer>, float, std::less<ActorRef<IPlayer> >, std::allocator<std::pair<ActorRef<IPlayer> const, float> > > m_playerTimeLeft;
 
-    void UpdatePlayerAttacks();m
+    void UpdatePlayerAttacks();
     float GetMinimumTimeRemaining();
   public:
     BearChest();
